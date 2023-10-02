780492fe64a88936afbb40c999e228df27a70421
georgmarts <118880240+georgmarts@users.noreply.github.com>
2023-10-02 17:29:44 +0500

    Delete src directory

--- src/utils/validateUser.js
+++
@@ -1,7 +0,0 @@
-import { supabase } from "../lib/supabaseClient"
-
-
-export async function validateUser(email, password) {
-        const data = supabase.from('SNUsers').select().eq('email', email).eq('password', password)
-        return data
-}
