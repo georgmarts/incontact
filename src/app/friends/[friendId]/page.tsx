89fb5e3758af0de334be21bef0307f8c04654dc2
georgmarts <118880240+georgmarts@users.noreply.github.com>
2023-10-07 01:06:56 +0500

    Update page.tsx

--- src/app/friends/[friendId]/page.tsx
+++ src/app/friends/[friendId]/page.tsx
@@ -3,6 +3,7 @@ import FriendPosts from "@/components/Friend/FriendPosts"
 import FriendProfile from "@/components/Friend/FriendProfile"
 import Notification from "@/components/Notification"
 import ShareModal from '@/components/ShareModal'
+import Header from '@/components/Header'
 
 type Props = {
     params: {
@@ -17,7 +18,7 @@ export default function Page({params}: Props) {
         <Notification/>
         <ShareModal/>
         <Header>
-          <h3>Friend's wall</h3>
+          <h3>Friend wall</h3>
         </Header>
         <FriendProfile friendId={friendId}/>
         <FriendPosts friendId={friendId}/>
