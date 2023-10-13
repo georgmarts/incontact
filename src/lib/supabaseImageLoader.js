75cab197f683ae8ab99a77fb95416c919dec1576
georgmarts <118880240+georgmarts@users.noreply.github.com>
2023-10-13 21:29:39 +0500

    Delete src directory

--- src/lib/supabaseImageLoader.js
+++
@@ -1,7 +0,0 @@
-const projectId = 'fsmlpmfunvdgvvoanhds'
-
-export default function supabaseLoader({ src, width, quality }) {
-  return `https://${projectId}.supabase.co/storage/v1/render/image/public/${src}?width=${width}&quality=${
-    quality || 75
-  }`
-}
